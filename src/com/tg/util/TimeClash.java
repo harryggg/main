9d6f22ccca108d6d1a773da67e9f593fdf179732
harryggg <ma.tanghao@dhs.sg>
2017-10-25 03:07:20 +0800

    relocate

--- src/com/tg/util/TimeClash.java
+++ src/com/tg/util/TimeClash.java
@@ -9,32 +9,7 @@ import java.util.Collections;
 public class TimeClash {
 	private int START_INDEX = 0;
 	private ArrayList<Event> _scheduleCache;
-	
-	/**
-	 * Initialise arraylist which keeps track of all schedule timings
-	 */
-	public TimeClash() {
-		_scheduleCache = new ArrayList<Event>();
-	}
-	
-	/**
-	 * updates _scheduleCache with @param newCache, sorts schedules according to start time
-	 * @param newCache
-	 */
-	public void updateCache(ArrayList<Event> newCache) {
-		_scheduleCache = newCache;
-		Collections.sort(_scheduleCache, Sorters.sortStart());
-		updateClashes();
-	}
-	
-	/**
-	 * returns new _scheduleCache updated with hasClash flags
-	 * @return _scheduleCache
-	 */
-	public ArrayList<Event> getCache() {
-		return _scheduleCache;
-	}
-	
+
 	/**
 	 * Checks _scheduleCache if there are any Event that clashes with another
 	 */
@@ -63,6 +38,32 @@ public class TimeClash {
 				}
 			}
 		}
+	}
+	
+	
+	/**
+	 * Initialise arraylist which keeps track of all schedule timings
+	 */
+	public TimeClash() {
+		_scheduleCache = new ArrayList<Event>();
+	}
+	
+	/**
+	 * updates _scheduleCache with @param newCache, sorts schedules according to start time
+	 * @param newCache
+	 */
+	public void updateCache(ArrayList<Event> newCache) {
+		_scheduleCache = newCache;
+		Collections.sort(_scheduleCache, Sorters.sortStart());
+		updateClashes();
+	}
+	
+	/**
+	 * returns new _scheduleCache updated with hasClash flags
+	 * @return _scheduleCache
+	 */
+	public ArrayList<Event> getCache() {
+		return _scheduleCache;
 	}
 	
 	/**
